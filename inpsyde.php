5a4f4458d97fc675526469b5c3a825bdd8d9776a
mcolete <mcolete@katanatechworks.com>
2020-05-29 00:55:17 +0800

    Initial setup

---
+++ inpsyde.php
@@ -0,0 +1,36 @@
+<?php
+  /*
+    Plugin Name: Inpsyde Exam
+    Description: Ways to test skills for qualification
+    Author: Monico Colete
+    Version: 0.1
+    
+    
+
+  */
+
+
+
+
+/** 
+ * TODO:
+ * - Create custom Endpoint(Not a REST), so it's a SOAP
+ * - Call to REST endpoint to https://jsonplaceholder.typicode.com/users
+ * - Parse JSON, build table
+ * - Row contains id, name, username, arranged chronologically
+ * - (CSS beautify optional)
+ * - When clicking on any column, it should show more of user details(Not posts, etc)
+ * - Show max one user, reload when clicking on the other
+ * - Cache HTTP requests, rationale should be documented in the README 
+ * - Error Handling for the external HTTP requests
+ * - Navigation shouldn't be disrupted
+ * - Update README, in Markdown-formatting explain usage
+ * - And decisiions behind the implementation
+ * - Inpsyde code style (Use the installed package)
+ * - Automated tests
+ * - Create LICENSE file
+ * - Extra features(Optional)
+ * - Unit tests (Use Brain Monkey package)
+ * - Other tests (Optional)
+ * - Explain installed packages in README
+ **/
