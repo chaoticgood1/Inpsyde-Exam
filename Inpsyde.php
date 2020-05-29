2ccf84a6933b64a699be11e5b4ccc7771252d56b
mcolete <mcolete@katanatechworks.com>
2020-05-30 07:51:12 +0800

    Refactor: Renamed file names

---
+++ Inpsyde.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Plugin Name: Inpsyde Exam
+ * Description: Ways to test skills for qualification
+ * Author: Monico Colete
+ * Version: 0.1
+ * PHP Version 7.2
+ * 
+ * @category Plugin_Inpsyde
+ * @package  Plugin_Inpsyde
+ * @author   Monico Colete <colete_nico@yahoo.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
+
+define('INPSYDE_PATH', plugin_dir_path(__FILE__));
+
+/**
+ * Implements Exam
+ * 
+ * @category Plugin_Inpsyde
+ * @package  Plugin_Inpsyde
+ * @author   Monico Colete <colete_nico@yahoo.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
+class Inpsyde
+{
+
+    public function init()
+    {
+        add_filter( 'template_include', array( $this, 'include_template' ) );
+        add_filter( 'init', array( $this, 'rewrite_rules' ) );
+        $this->addUsersScript();
+    }
+
+    public function include_template( $template )
+    {
+        $request = $_SERVER['REQUEST_URI'];
+        if ($request == "/users") {
+            return INPSYDE_PATH . '/src/page/UserPage.php';
+        }
+    }
+
+    public function flush_rules()
+    {
+        $this->rewrite_rules();
+        flush_rewrite_rules();
+    }
+
+    public function rewrite_rules()
+    {
+        add_rewrite_rule( 'account/(.+?)/?$', 'index.php?account_page=$matches[1]', 'top');
+        add_rewrite_tag( '%account_page%', '([^&]+)' );
+    }
+
+    public function addUsersScript() {
+        add_action('wp_enqueue_scripts', function() {
+            wp_enqueue_script('users.js', 
+              plugins_url('/js/users.js', __FILE__), array('jquery'),
+              0.1, false);
+        });
+        // TODO: Check if the Javascript is loaded?
+    }
+}
+
+add_action('plugins_loaded', array(new Inpsyde, 'init'));
+
+// One time activation functions
+register_activation_hook(plugins_url('Inpsyde.php'), array(new Inpsyde, 'flush_rules'));
+
+
+/** 
+ * TODO:
+ * - Create custom Endpoint(Not a REST), it is a simple routing...
+ * - Call to REST endpoint to https://jsonplaceholder.typicode.com/users
+ * - Parse JSON, build table
+ * - Row contains id, name, username, arranged chronologically
+ * - (CSS beautify optional)
+ * - When clicking on any column, it should show more of user details(Not posts, etc)
+ * - Show max one user, reload when clicking on the other
+ * - Cache HTTP requests, rationale should be documented in the README 
+ * - Error Handling for the external HTTP requests
+ * - Navigation shouldn't be disrupted
+ * - Update README, in Markdown-formatting explain usage
+ * - And decisiions behind the implementation
+ * - Inpsyde code style (Use the installed package)
+ * - Automated tests
+ * - Create LICENSE file
+ * - Extra features(Optional)
+ * - Unit tests (Use Brain Monkey package)
+ * - Other tests (Optional)
+ * - Explain installed packages in README
+ **/
+?>
---
+++ src/Users.php
@@ -0,0 +1,26 @@
+<?php
+class Users 
+{
+    public const USERS_API = "https://jsonplaceholder.typicode.com/users";
+
+    public function getData() // Rename? Later
+    {
+        // TODO: Handle error not getting
+        // - Long loading time
+        // - Cache result
+        $response = Users::get(Users::USERS_API);
+        return json_decode($response);
+    }
+
+    public static function get($url, $header = array(), $params = array()) 
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url.http_build_query($params));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+    }
+}
+?>
