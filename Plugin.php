d2f4ce21a2e6bb9310c2ad295295b8081c7493bb
mcolete <mcolete@katanatechworks.com>
2020-06-25 19:42:31 +0800

    Removed TODO

--- Plugin.php
+++ Plugin.php
@@ -21,18 +21,3 @@ add_action('plugins_loaded', function () {
     $inpsyde = new Inpsyde();
     $inpsyde->init();
 });
-
-/** TODO */
-// Rethinking the solutions
-// #phpDoc:
-//  - Put documentation to all classess and functions
-// #use WordPress API, not 3rd Party like Guzzle: Replace
-//  - Remove all Guzzle instances
-//  - Remove in composer
-//  - Find a way to implement HTTP caching in WP API
-//  - I think it is automated, but have to find a explicit way to do it
-// #Unit Tests, no User Tests
-//  - Should I add more unit tests?
-//  - I think what I did was not a Unit Tests?
-//  - What does "no User Tests" mean?
-// Ask for clarification, faster than guessing
